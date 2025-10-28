9dcaa19d5bbf22ae0fc86620584803c55d4f6fd9
WANDEX <15724752+WANDEX@users.noreply.github.com>
2025-10-28 19:35:12 +0300

    [wndx_sane_funcs.cmake] cleanup, prefix all defined functions,
    wndx_sane_tgt_add_check_cxx_compiler_flag():
    wrapper function with consecutive: check, apply.
    receives same arguments as target_compile_options().
    check_cxx_compiler_flag(), target_compile_options().

--- cmake/wndx_sane_funcs.cmake
+++ cmake/wndx_sane_funcs.cmake
@@ -30,54 +30,42 @@ function(wndx_sane_path arg_VAR_NAME) ## ARGN
 endfunction()
 
 ## define variables to be able to use short if conditions
-## if(${NOT_Release})
-function(not_build_type ARG)
-  if(NOT CMAKE_BUILD_TYPE STREQUAL ${ARG})
-    set(NOT_${ARG} TRUE  PARENT_SCOPE)
-  else()
-    set(NOT_${ARG} FALSE PARENT_SCOPE)
-  endif()
-endfunction(not_build_type)
+## if(Release)
 ## creation of variables to be able to use them later
-not_build_type(Release)
-not_build_type(Debug)
+## wndx_sane_build_type(Release)
+## wndx_sane_build_type(Debug)
+function(wndx_sane_build_type ARG)
+  if(CMAKE_BUILD_TYPE STREQUAL ${ARG})
+    set(${ARG} TRUE  PARENT_SCOPE)
+  else()
+    set(${ARG} FALSE PARENT_SCOPE)
+  endif()
+endfunction(wndx_sane_build_type)
 
 
 ## define variables to be able to use short & readable if conditions
-## if(${Clang_COMP} OR ${GNU_COMP})
-function(under_compiler ARG)
+## creation of variables to be able to use them later
+## wndx_sane_under_compiler(GNU)
+## wndx_sane_under_compiler(Clang)
+## wndx_sane_under_compiler(AppleClang)
+## wndx_sane_under_compiler(MSVC) # cmake has short MSVC  (so this is added for consistency!)
+## if(GNU_COMP OR Clang_COMP OR AppleClang_COMP)
+function(wndx_sane_under_compiler ARG)
   if( (  CMAKE_C_COMPILER_ID STREQUAL ${ARG}) OR
       (CMAKE_CXX_COMPILER_ID STREQUAL ${ARG}) )
-    if(${NOT_Release})
-      message(NOTICE ">> ${CMAKE_PROJECT_NAME} UNDER ${ARG}")
-    endif()
+    # message(DEBUG "wndx_sane_under_compiler() ${CMAKE_PROJECT_NAME} UNDER ${ARG}")
     set(${ARG}_COMP TRUE  PARENT_SCOPE)
   else()
     set(${ARG}_COMP FALSE PARENT_SCOPE)
   endif()
-endfunction(under_compiler)
-## creation of variables to be able to use them later
-under_compiler(GNU)
-under_compiler(Clang)
-under_compiler(AppleClang)
-under_compiler(MSVC)   # cmake has short MSVC  (so this is added for consistency!)
+endfunction(wndx_sane_under_compiler)
 
 
-## https://cmake.org/cmake/help/latest/module/CheckCXXCompilerFlag.html
-include(CheckCXXCompilerFlag) # -> check_cxx_compiler_flag()
-
-## NOTE: only one flag is supported as function argument!
-## usage target_add_check_cxx_compiler_flag(target -Wstrict-null-sentinel )
-function(target_add_check_cxx_compiler_flag target flag)
-  check_cxx_compiler_flag(${flag} HAS_${flag})
-  if(HAS_${flag})
-    target_compile_options(${target} INTERFACE ${flag})
-  endif()
-endfunction(target_add_check_cxx_compiler_flag)
-
-
-## recursively include all sub-directories of the given dir
-function(include_subdirs arg_dir)
+## recursively include all sub-directories of the given dir.
+## NOTE: recursive approach is strongly discouraged!
+##       manual and target based include are preferred!
+function(wndx_sane_include_subdirs arg_dir)
+  set(fun "wndx_sane_include_subdirs()")
   file(GLOB_RECURSE recurse_rpaths LIST_DIRECTORIES true
     RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}" "${arg_dir}/$")
   ## ^ list only sub-directories of the given dir using relative path
@@ -87,17 +75,33 @@ function(include_subdirs arg_dir)
     file(REAL_PATH "${rel_path}" abs_path BASE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
     ## double check that this is directory
     if(NOT IS_DIRECTORY "${abs_path}")
-      if(${NOT_Release})
-        message(DEBUG "(SKIP) THIS IS NOT DIRECTORY: ${abs_path}")
-      endif()
+      message(DEBUG "${fun} (SKIP) THIS IS NOT DIRECTORY: ${abs_path}")
       continue()
     endif()
     ## include dir
     include_directories("${abs_path}")
     ## see which dirs are included in the build process
-    if(${NOT_Release})
-      message(DEBUG "included subdir: ${abs_path}")
-    endif()
+    message(DEBUG "${fun} included subdir: ${abs_path}")
   endforeach(rel_path)
-endfunction(include_subdirs)
+endfunction(wndx_sane_include_subdirs)
+
+## https://cmake.org/cmake/help/latest/module/CheckCXXCompilerFlag.html
+include(CheckCXXCompilerFlag) # -> check_cxx_compiler_flag()
+
+## wrapper function with consecutive: check, apply.
+## receives same arguments as target_compile_options().
+## check_cxx_compiler_flag(), target_compile_options().
+function(wndx_sane_tgt_add_check_cxx_compiler_flag) ## args
+  set(arg_TARGET ${ARGV0})
+  set(arg_SCOPE  ${ARGV1})
+  list(SUBLIST ARGV 2 -1 arg_FLAGS) # ARGV leftovers
+  set(fun "wndx_sane_tgt_add_check_cxx_compiler_flag()")
+  foreach(flag ${arg_FLAGS})
+    # message(DEBUG "${fun} ${flag}")
+    check_cxx_compiler_flag(${flag} HAS_${flag})
+    if(HAS_${flag})
+      target_compile_options(${arg_TARGET} ${arg_SCOPE} ${flag})
+    endif()
+  endforeach(flag)
+endfunction(wndx_sane_tgt_add_check_cxx_compiler_flag)
 
