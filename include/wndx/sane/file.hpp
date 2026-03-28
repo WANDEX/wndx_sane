1636ab844e874c88f7af8875a2430f45ca8eab46
WANDEX <15724752+WANDEX@users.noreply.github.com>
2026-03-28 20:57:23 +0300

    extracted from wndx::mqlqd File & File tests

--- include/wndx/sane/file.hpp
+++ include/wndx/sane/file.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "aliases.hpp"        // IWYU pragma: keep
+#include "aliases.hpp" // IWYU pragma: keep
 
-#include "wndx/sane/path.hpp" // IWYU pragma: keep
+#include "path.hpp"    // IWYU pragma: keep
 
+#include <cstring>     // std::memcmp()
 #include <filesystem>
 
 
@@ -15,5 +16,166 @@ namespace wndx::sane::file {
                        fs::perms       perms = fs::perms::group_all,
                        bool            force = false) noexcept;
 
+/// \brief length chosen arbitrarily to fit most of of the file names into.
+static constexpr std::size_t fname_max_len{ 79 };
+
+/// \brief struct file info.
+struct Finfo
+{
+  std::size_t m_block_size{ 0 }; // NOLINTNEXTLINE(*-avoid-c-arrays)
+  char        m_fname[fname_max_len]{ "sane_default_file_name\0" };
+};
+
+class File
+{
+public:
+  /// to read/write/store all files as binary data with the unified underlying
+  /// type across the project code base and across devices.
+  /// NOTE: tried other types, specifically:
+  /// u8 - unsigned char type => break the logic:
+  /// printing/writing binary file contents (send/recv maybe also, but not sure)
+  /// Currently I do not see any benefit in wasting time on making this
+  /// possible using u8. Probably some extra handling required, which only
+  /// overcomplicate code. (so why bother?)
+  using char_type = char;
+
+  // clang-format off
+  static constexpr auto openmode_r{
+    std::ios::in  | std::ios::binary | std::ios::ate
+  };
+  static constexpr auto openmode_w{
+    std::ios::out | std::ios::binary
+  };
+  // clang-format on
+
+  File()                       = delete;
+  File(File&&)                 = default;
+  File(File const&)            = default;
+  File& operator=(File&&)      = delete;
+  File& operator=(File const&) = delete;
+  ~File() noexcept;
+
+  /// \brief construct class instance from the file path & its size.
+  explicit File(fs::path fpath, std::size_t sz) noexcept;
+
+  /// \brief construct class instance from the file info structure.
+  explicit File(Finfo const& finfo, fs::path const& dpath) noexcept;
+
+  /// \brief cleanup facilities for the heap.
+  void clean_heap() noexcept;
+
+  /// \brief convert essentials of the instance into file info structure.
+  [[nodiscard]] Finfo to_finfo() const noexcept;
+
+  /// \brief allocate required space in the heap for the file contents.
+  [[nodiscard]] rc alloc() noexcept;
+
+  /// \brief write file to the disk.
+  [[nodiscard]] rc write() const noexcept;
+
+  /// \brief read file contents into the memory block.
+  [[nodiscard]] rc read() const noexcept;
+
+  /// \brief alloc space & read file contents into the memory block.
+  [[nodiscard]] rc alloc_and_read() noexcept;
+
+  /// \brief copy file to the destination.
+  [[nodiscard]] rc copy_to(fs::path const& dst) noexcept;
+
+  /// \brief move file to the destination.
+  [[nodiscard]] rc move_to(fs::path const& dst) noexcept;
+
+  /// \brief print file contents (like a UNIX cat utility).
+  void print() const noexcept;
+
+  /// \brief path to the file.
+  [[nodiscard]] fs::path path() const noexcept { return m_fpath; }
+
+  /// \brief size of contiguous memory block required for the file.
+  [[nodiscard]] constexpr std::size_t size() const noexcept
+  {
+    return m_block_size;
+  }
+
+  /// \brief memory block.
+  [[nodiscard]] constexpr char_type* memory() const noexcept { return m_block; }
+
+  /// \brief uses compiler intrinsics for the efficient memory comparison.
+  ///
+  /// \see https://en.cppreference.com/w/cpp/string/byte/memcmp
+  [[nodiscard]] static int memcmp(char_type const* const lhs,
+                                  char_type const* const rhs, std::size_t count)
+  {
+    return std::memcmp(lhs, rhs, count); // not constexpr!
+  }
+
+  /// \brief uses compiler intrinsics for comparing files memory blocks.
+  [[nodiscard]] int memcmp(char_type const* const rhs) const noexcept
+  {
+    return memcmp(memory(), rhs, size());
+  }
+
+  /// \brief allow conversion of the memory block pointer to bool.
+  constexpr explicit operator bool() const noexcept
+  {
+    return m_block != nullptr;
+  }
+
+  constexpr bool operator!=(File const& rhs) const noexcept
+  {
+    return !m_block || !rhs.m_block || size() != rhs.size();
+  }
+
+  constexpr bool operator<(File const& rhs) const noexcept
+  {
+    return operator!=(rhs) && size() < rhs.size();
+  }
+
+  constexpr bool operator>(File const& rhs) const noexcept
+  {
+    return operator!=(rhs) && size() > rhs.size();
+  }
+
+  /// \brief equality of the files contents (compare memory blocks).
+  ///
+  /// memory comparison is the most expensive operation,
+  /// so we compare memory at the very end, after all other conditions.
+  constexpr bool operator==(File const& rhs) const noexcept
+  {
+    return !operator!=(rhs) && memcmp(rhs.memory()) == 0;
+  }
+
+  constexpr bool operator<=(File const& rhs) const noexcept
+  {
+    return operator<(rhs) || operator==(rhs);
+  }
+
+  constexpr bool operator>=(File const& rhs) const noexcept
+  {
+    return operator>(rhs) || operator==(rhs);
+  }
+
+private:
+  std::size_t m_block_size{ 0 };
+  fs::path    m_fpath{};
+  char_type*  m_block{ nullptr }; // memory block -> contiguous chunk of memory.
+};
 
 } // namespace wndx::sane::file
+
+
+/// \brief fmt formatter specialization for Finfo.
+template <>
+struct fmt::formatter<wndx::sane::file::Finfo> : formatter<string_view>
+{
+  auto format(wndx::sane::file::Finfo const& f, format_context& ctx) const
+      -> format_context::iterator;
+};
+
+/// \brief fmt formatter specialization for File.
+template <>
+struct fmt::formatter<wndx::sane::file::File> : formatter<string_view>
+{
+  auto format(wndx::sane::file::File const& f, format_context& ctx) const
+      -> format_context::iterator;
+};
---
+++ tests/units/file.t.cpp
@@ -0,0 +1,213 @@
+#include "wndx/sane/file.hpp"
+
+#include <gtest/gtest.h>
+
+#include <cstring> // memcpy
+#include <filesystem>
+#include <string>
+#include <system_error>
+
+
+namespace wndx::sane {
+
+[[nodiscard]]
+auto make_tmp_dir() noexcept
+{
+  rc              rc{ rc::FAILURE };
+  std::error_code ec{};
+  fs::path        tmp_dir{ fs::temp_directory_path(ec) };
+  if (ec) {
+    WNDX_LOG(LL::ERRO, "[FAIL] fs::temp_directory_path(ec) -> v:{} m:{}\n",
+             ec.value(), ec.message());
+  }
+  tmp_dir /= "wndx";
+  rc = wndx::sane::file::mkdir(tmp_dir, fs::perms::all);
+  EXPECT_TRUE(rc == rc::SUCCESS);
+  tmp_dir /= "sane";
+  rc = wndx::sane::file::mkdir(tmp_dir, fs::perms::all);
+  EXPECT_TRUE(rc == rc::SUCCESS);
+  return tmp_dir.string();
+}
+
+static auto const g_tmp_dir{ make_tmp_dir() };
+
+class File_test : public ::testing::Test
+{
+public:
+  bool m_do_alloc{ true };
+
+  /// \brief allocate space, read file contents, check that memory is allocated.
+  // NOLINTNEXTLINE(readability-function-cognitive-complexity)
+  void alloc_read_check(file::File& file) const noexcept
+  {
+    if (m_do_alloc) {
+      /// read file and check that memory is allocated.
+      ASSERT_TRUE(file.alloc_and_read() == rc::SUCCESS);
+      ASSERT_TRUE(file.memory() != nullptr);
+      ASSERT_TRUE(file);
+      ASSERT_FALSE(!file);
+    } else {
+      ASSERT_TRUE(file.memory() == nullptr);
+      ASSERT_FALSE(file);
+      ASSERT_TRUE(!file);
+    }
+  }
+
+  /// \brief helper function to create File from the tests data files.
+  [[nodiscard]]
+  file::File
+  alloc_file(std::string const& file_name = "ascii_1.txt") const noexcept
+  {
+    fs::path dp;
+    fs::path fp;
+    EXPECT_NO_THROW({
+      dp = "./data";
+      fp = dp / file_name;
+    });
+    file::File File(fp, fs::file_size(fp));
+    alloc_read_check(File);
+    return File;
+  }
+
+  [[nodiscard]]
+  static fs::path get_tmp_dir() noexcept
+  {
+    return g_tmp_dir;
+  }
+
+  [[nodiscard]]
+  file::File tmp_file(std::string const& fname) const noexcept
+  {
+    auto     file{ alloc_file(fname) };
+    fs::path tmp_fpath{ get_tmp_dir() / fname };
+    WNDX_LOG(LL::DBUG, "tmp_fpath: {}\n", tmp_fpath.string());
+    EXPECT_TRUE(file.copy_to(tmp_fpath) == rc::SUCCESS);
+    return file;
+  }
+};
+
+TEST_F(File_test, ctor_file_size)
+{
+  auto file{ alloc_file() };
+  ASSERT_EQ(file.size(), fs::file_size(file.path()));
+}
+
+TEST_F(File_test, File_to_finfo)
+{
+  auto const file_name{ "ascii_2.txt" }; // NOLINT(readability-qualified-auto)
+  auto const file{ alloc_file(file_name) };
+  auto const finfo{ file.to_finfo() };
+  // NOLINTNEXTLINE(*-array-to-pointer-decay, hicpp-no-array-decay)
+  ASSERT_EQ(file_name, std::string(finfo.m_fname));
+}
+
+TEST_F(File_test, ctor_finfo_path)
+{
+  auto const file_name{ "ascii_3.txt" }; // NOLINT(readability-qualified-auto)
+  auto const len{ std::string(file_name).length() };
+  auto const file{ alloc_file(file_name) };
+  auto const finfo{ file.to_finfo() };
+  // NOLINTNEXTLINE(*-array-to-pointer-decay, hicpp-no-array-decay)
+  ASSERT_EQ(file_name, std::string(finfo.m_fname));
+  ASSERT_EQ(file.size(), fs::file_size(file.path()));
+
+  file::Finfo finfo2;
+  finfo2.m_block_size = file.size();
+  finfo2.m_fname[len] = '\0'; // NOLINT(*-constant-array-index)
+  std::memcpy(&finfo2.m_fname, file_name, len);
+  // NOLINTNEXTLINE(*-array-to-pointer-decay, hicpp-no-array-decay)
+  ASSERT_EQ(std::string(finfo.m_fname), std::string(finfo2.m_fname));
+}
+
+TEST_F(File_test, compare_nullptr)
+{
+  m_do_alloc = false;
+  auto file1{ alloc_file() };
+  auto file2{ alloc_file() };
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);  // assume not eq when nullptr!
+  ASSERT_FALSE(file1 == file2); // due to nullptr!
+  ASSERT_FALSE(file1 <= file2);
+  ASSERT_FALSE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_eq)
+{
+  auto file1{ alloc_file() };
+  auto file2{ alloc_file() };
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_FALSE(file1 != file2);
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_TRUE(file1 <= file2);
+  ASSERT_TRUE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_lt)
+{
+  auto file1{ alloc_file("ascii_1.txt") };
+  auto file2{ alloc_file("ascii_2.txt") };
+  ASSERT_TRUE(file1 < file2);
+  ASSERT_FALSE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);
+  ASSERT_FALSE(file1 == file2);
+  ASSERT_TRUE(file1 <= file2);
+  ASSERT_FALSE(file1 >= file2);
+}
+
+TEST_F(File_test, compare_gt)
+{
+  auto file1{ alloc_file("ascii_3.txt") };
+  auto file2{ alloc_file("ascii_2.txt") };
+  ASSERT_FALSE(file1 < file2);
+  ASSERT_TRUE(file1 > file2);
+  ASSERT_TRUE(file1 != file2);
+  ASSERT_FALSE(file1 == file2);
+  ASSERT_FALSE(file1 <= file2);
+  ASSERT_TRUE(file1 >= file2);
+}
+
+TEST_F(File_test, copy_tmp_file)
+{
+  char const* fname{ "ascii_1.txt" };
+  auto const  file1{ alloc_file(fname) };
+  auto const  file2{ tmp_file(fname) };
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_NE(file1.path(), file2.path());
+}
+
+TEST_F(File_test, move_tmp_file)
+{
+  char const* fname1{ "ascii_1.txt" };
+  char const* fname2{ "moved_ascii_1.txt" };
+  auto const  file1{ alloc_file(fname1) };
+  auto        file2{ tmp_file(fname1) };
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_NE(file1.path(), file2.path());
+  ASSERT_TRUE(file2.move_to({ get_tmp_dir() / fname2 }) == rc::SUCCESS);
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_NE(file1.path(), file2.path());
+}
+
+TEST_F(File_test, copy_and_move)
+{
+  char const* fname1{ "ascii_1.txt" };
+  char const* fname2{ "moved_ascii_1.txt" };
+  auto const  file1{ alloc_file(fname1) };
+  auto        file2{ alloc_file(fname1) };
+  fs::path    fpath1{ get_tmp_dir() / fname1 };
+  fs::path    fpath2{ get_tmp_dir() / fname2 };
+  WNDX_LOG(LL::DBUG, "fpath1: {}\n", fpath1.string());
+  WNDX_LOG(LL::DBUG, "fpath2: {}\n", fpath2.string());
+  ASSERT_NE(fpath1, fpath2);
+  ASSERT_TRUE(file2.copy_to(fpath1) == rc::SUCCESS);
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_EQ(file2.path(), fpath1);
+  ASSERT_TRUE(file2.move_to(fpath2) == rc::SUCCESS);
+  ASSERT_TRUE(file1 == file2);
+  ASSERT_NE(file1.path(), file2.path());
+  ASSERT_EQ(file2.path(), fpath2);
+}
+
+} // namespace wndx::sane
